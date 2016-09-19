ec925fb48471ec6eec0adf36c5e22bf44887f5e1
syzer3@gmail.com <syzer3@gmail.com>
2016-09-19 22:00:10 +0200

    making cors to spark.io

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,29 @@
+const rp = require('request-promise')
 const express = require('express')
-const app = express()
+const cors = require('cors')
+
+let app = express()
+
+app.use(cors({
+    origin: 'localhost:3006'
+}))
 
 app.get('/dupa', (req, res, next) => {
-    console.log('dupa')
-    res.json({'dupa':42})
+
+    rp({
+        uri: 'https://api.spark.io/v1/devices/240034001147343339383037',
+        qs: {
+            access_token: '800efcf08f1f961e4438ece0da1a20bc5ccfb4f4'
+        },
+        headers: {
+            // 'Authorization': 'Bearer <authentication code>'
+        },
+        json: true
+    })
+        .catch(err => res.status(500).json(err))
 })
+
+app.use(express.static('public'))
 
 const server = app.listen(process.env.PORT || 3006, () => {
     console.log(`Express is listening to ${server.address().port}`)
--- frontend/index.js
+++ frontend/index.js
@@ -4,24 +4,15 @@ import displayChart from './displayChart'
 
 // expose jquery
 require('expose?$!expose?jQuery!jquery')
-require('bootstrap')
+require('bootstrap-webpack')
 require('./style.css')
 
 let eventSource = connect()
 const pplCount = document.getElementById('pplCount')
 
-rp({
-    uri: 'https://api.spark.io/v1/devices/240034001147343339383037',
-    qs: {
-        access_token: '800efcf08f1f961e4438ece0da1a20bc5ccfb4f4'
-    },
-    headers: {
-        // 'Content-Type': 'application/json',
-        // 'Authorization': 'Bearer <authentication code>'
-    }
-})
-    .then(console.warn)
-    .catch(console.log)
+rp(`http://localhost:3006/dupa`)
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
 
 eventSource.addEventListener('humancount', (e) => {
     let parsedData = JSON.parse(e.data)
